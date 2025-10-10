84977d6e3c8841e08eaeb22da993dc2befabffed
boudjelaba <56698791+boudjelaba@users.noreply.github.com>
2025-10-10 08:40:51 +0200

    Delete simulation_rpi directory

--- simulation_rpi/btn.py
+++
@@ -1,8 +0,0 @@
-import MockGPIO as GPIO
-from config_gpio import initialiser_broches
-import time
-
-initialiser_broches()
-GPIO.simulate_button_press(17)
-time.sleep(1)
-GPIO.simulate_button_release(17)
